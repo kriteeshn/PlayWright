d3859b09c52105e50f7c473d11dbe2b51f34ce7d
kriteeshn <155950044+kriteeshn@users.noreply.github.com>
2025-03-19 15:02:55 +0530

    dish changed

--- datatypes.js/Array.js
+++ datatypes.js/Array.js
@@ -3,7 +3,7 @@
 // let arrandnum = [0,1,2,3,"sachin","dhoni","kane",[1,2,3,4]];
 // console.log(arrandnum[7][2]);
 
-dish.push("mysore masal");
+dish.push("vadaa");
 console.log(dish)
 
 // dish.pop();
